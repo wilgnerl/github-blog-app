cc6ce4fc131efaa4d47e04bdc1676f4501a51e79
wilgnerl <wilgnerl@al.insper.edu.br>
2023-01-22 19:55:16 -0300

    chore: setting theme, fonts and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
       <h1>Hello world</h1>
-    </div>
+    </ThemeProvider>
   )
 }
